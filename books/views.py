3c04508bf7a57fb410605da2d949884a28879d24
Marti <52050778+github-marti@users.noreply.github.com>
2020-01-26 14:55:53 -0800

    changed load to render and created html template for index

--- books/views.py
+++ books/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from .models import Book
 
 # Create your views here.
 def index(request):
     latest_book_list = Book.objects.order_by('-add_date')[:5]
-    output = ', '.join(['{} by {}'.format(q.title, q.author) for q in latest_book_list])
-    return HttpResponse(output)
+    context = {
+        'latest_book_list': latest_book_list
+    }
+    return render(request, 'books/index.html', context)
