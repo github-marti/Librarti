8ed420184b45944fdcd566f8930a925dcfb116d6
Marti <52050778+github-marti@users.noreply.github.com>
2020-01-26 14:34:29 -0800

    /books now redirects to index page as well

--- books/views.py
+++ books/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Book
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Welcome to the future home of Librarti.")
+    latest_book_list = Book.objects.order_by('-add_date')[:5]
+    output = ', '.join(['{} by {}'.format(q.title, q.author) for q in latest_book_list])
+    return HttpResponse(output)
