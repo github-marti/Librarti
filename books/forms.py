f89dba66bdb3669ecdf95a59571c9289fde26af4
Marti <52050778+github-marti@users.noreply.github.com>
2020-01-28 10:24:03 -0800

    added forms.py

---
+++ books/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+from .models import Book, Review
+
+class BookForm(forms.ModelForm):
+
+    class Meta:
+        model = Book
+        fields = ('title', 'author', 'synopsis')
--- books/views.py
+++ books/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect
-from .models import Book, Review
 from django.urls import reverse
 from django.views import generic
+from .models import Book, Review
 
 # Create your views here.
 class IndexView(generic.ListView):
