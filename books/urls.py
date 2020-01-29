3eeea5b8d36fdd7ba185d8abde832b2ba61cacca
Marti <52050778+github-marti@users.noreply.github.com>
2020-01-28 18:14:03 -0800

    changed book detail url to include book/

--- books/urls.py
+++ books/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'books'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
-    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('book/<int:pk>/', views.DetailView.as_view(), name='detail'),
     path('book/new/', views.new_book, name='new_book'),
     path('book/<int:book_id>/edit_book', views.update_book, name='update_book'),
     path('book/<int:book_id>/edit_review', views.update_review, name='update_review')
