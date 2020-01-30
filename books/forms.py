937fb64e2fe1923900beb40f5638c144fca427ba
Marti <52050778+github-marti@users.noreply.github.com>
2020-01-29 19:00:08 -0800

    added widgets to modelforms to customize form look

--- books/forms.py
+++ books/forms.py
@@ -1,14 +1,23 @@
-from django import forms
+from django.forms import ModelForm, TextInput, Textarea
 from .models import Book, Review
 
-class BookForm(forms.ModelForm):
+class BookForm(ModelForm):
 
     class Meta:
         model = Book
         fields = ('title', 'author', 'image', 'synopsis')
+        widgets = {
+            'title': TextInput(attrs={'class': 'form-control'}),
+            'author': TextInput(attrs={'class': 'form-control'}),
+            'image': TextInput(attrs={'class': 'form-control'}),
+            'synopsis': Textarea(attrs={'class': 'form-control'})
+            }
 
-class ReviewForm(forms.ModelForm):
+class ReviewForm(ModelForm):
 
     class Meta:
         model = Review
         fields = ('stars', 'thoughts')
+        widgets = {
+            'thoughts': Textarea(attrs={'class': 'form-control'})
+        }
