91eee035998c2a32dc2eaad70bccd4f7ecb248a2
Marti <52050778+github-marti@users.noreply.github.com>
2020-02-01 11:37:56 -0800

    moved browse up into header

--- books/forms.py
+++ books/forms.py
@@ -28,7 +28,7 @@ class StarForm(ModelForm):
         model = Review
         fields = ('stars',)
         labels = {
-            'stars': 'Search by Review',
+            'stars': 'by Review',
         }
         widgets = {
             'stars': Select(attrs={'class': 'search-form'})
